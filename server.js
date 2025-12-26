5f0caef08dfc6636144136cf8549d7ff69bdd793
Shiva rama krishna Kunuku <kkrishna@pc.factset.com>
2025-12-26 23:22:14 +0530

    added pg

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const app = express();
+const { Pool } = require('pg');
 
 const PORT = 3000;
 
 // Middleware to parse JSON
 app.use(express.json());
 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+
 // Route
 app.get("/", (req, res) => {
   res.send("Hello Express with Yarn! 🚀");
+});
+
+app.get('/users', async (req, res) => {
+  const result = await pool.query('SELECT * FROM users');
+  res.json(result.rows);
 });
 
 // Start server
